99bd1bb392bbac57317093c3309dc95bd6f02445
aerth <aerth@riseup.net>
2017-04-10 23:57:42 -0700

    gofmt

--- main.go
+++ main.go
@@ -12,7 +12,10 @@ var (
 )
 
 func main() {
-	if len(os.Args) > 1 {println("Demo binary (safe to delete). Learn more: [https://github.com/aerth/make.go]");os.Exit(111)}
+	if len(os.Args) > 1 {
+		println("Demo binary (safe to delete). Learn more: [https://github.com/aerth/make.go]")
+		os.Exit(111)
+	}
 	fmt.Printf("%s %s (runtime: %s)\n", os.Args[0], version, runtime.Version())
 	_ = net.Interfaces
 }
