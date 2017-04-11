d1ca07e60458a3e57882ddf75f1990b58fc518bb
aerth <aerth@riseup.net>
2017-04-10 23:56:35 -0700

    add "net" package to main.go so there is some evidence if we are using CGO or not

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os"
 	"runtime"
 )
@@ -11,5 +12,7 @@ var (
 )
 
 func main() {
+	if len(os.Args) > 1 {println("Demo binary (safe to delete). Learn more: [https://github.com/aerth/make.go]");os.Exit(111)}
 	fmt.Printf("%s %s (runtime: %s)\n", os.Args[0], version, runtime.Version())
+	_ = net.Interfaces
 }
