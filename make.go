79e0ece55d80a8a4f8d44ae8027f6d4726f425f0
aerth <aerth@riseup.net>
2017-04-10 19:28:10 -0700

    chmod +x main.go     add #!shebang,     add verbosity flag,     rename release flag to -all,     add -c for change directory

--- make.go
+++ make.go
@@ -1,4 +1,5 @@
-// +build ignore
+///bin/true; exec /usr/bin/env go run "$0" "$@"; exit "$?"
+//+build ignore
 
 package main
 
@@ -11,10 +12,7 @@ import (
 	"runtime"
 	"strings"
 	"sync"
-)
-
-const (
-	commandName = "example-command"
+	"bytes"
 )
 
 type platform struct {
@@ -28,33 +26,32 @@ type binary struct {
 	targets []platform
 }
 
-// Name returns the name of the binary with a certain format for a platform.
-func (bin binary) Name(os, arch string) string {
-	s := fmt.Sprintf("%s_%s_%s-%s", bin.name, bin.version, os, arch)
-	if os == "windows" {
-		s = s + ".exe"
-	}
-	return s
-}
-
-// Names returns the name of the binary for all the target platforms.
-func (bin binary) Names() []string {
-	names := make([]string, len(bin.targets))
-	for i, t := range bin.targets {
-		names[i] = bin.Name(t.os, t.arch)
-	}
-	return names
-}
-
 var (
-	release   = flag.Bool("release", false, "build binaries for all target platforms")
+	release   = flag.Bool("all", false, "build binaries for all target platforms")
+	verbose   = flag.Bool("v", false, "print build output")
 	clean     = flag.Bool("clean", false, "remove all created binaries from current directory")
 	buildOS   = flag.String("os", runtime.GOOS, "set operating system to build for")
 	buildArch = flag.String("arch", runtime.GOARCH, "set architecture to build for")
+	chroot = flag.String("c", "", "change to directory before starting")
 )
 
+var rwd string
+
+func init(){
+	log.SetFlags(0)
+	var err error
+	rwd, err = os.Getwd()
+	if err != nil {
+		println(err.Error())
+		os.Exit(111)
+	}
+	if rwd != "" {
+		rwd+="/"
+	}
+}
+
 func usage() {
-	fmt.Fprintf(os.Stderr, "Usage: go run make.go [OPTIONS]\n\n")
+	fmt.Fprintf(os.Stderr, "Usage: make.go [OPTIONS] [GO_PACKAGE]\n\n")
 	fmt.Fprintln(os.Stderr, "OPTIONS:")
 	flag.PrintDefaults()
 }
@@ -62,18 +59,51 @@ func usage() {
 func main() {
 	flag.Usage = usage
 	flag.Parse()
+for _, fl := range flag.Args() {
+	if strings.HasPrefix(fl, "-"){
+		log.Fatalln("put flags before arguments")
+	}
+}
+var err error
+switch {
+case *chroot != "":
+		err = os.Chdir(*chroot)
+		if err != nil {
+			println(err.Error())
+			os.Exit(111)
+		}
+case len(flag.Args()) > 0:
+			err = os.Chdir(flag.Args()[0])
+			if err != nil {
+				println(err.Error())
+				os.Exit(111)
+			}
+	}
+
+
+	wd, err := os.Getwd()
+	if err != nil {
+		println(err.Error())
+		os.Exit(111)
+	}
 
 	bin := binary{
-		name: commandName,
+
+		name: getMainProjectName(wd),
 		// Change these according to the platforms you would like to support. A
 		// full list can be found here:
 		// https://golang.org/doc/install/source#environment
 		targets: []platform{
 			{os: "linux", arch: "386"}, {os: "linux", arch: "amd64"},
+			{os: "linux", arch: "arm"},
 			{os: "windows", arch: "386"}, {os: "windows", arch: "amd64"},
 			{os: "darwin", arch: "386"}, {os: "darwin", arch: "amd64"},
+			{os: "openbsd", arch: "386"}, {os: "openbsd", arch: "amd64"},
+			{os: "netbsd", arch: "386"}, {os: "netbsd", arch: "amd64"},
+			{os: "freebsd", arch: "386"}, {os: "freebsd", arch: "amd64"},
 		},
 	}
+
 	bin.version = getVersion()
 
 	if *release {
@@ -92,11 +122,15 @@ func main() {
 // getVersion returns the version of the project using git.
 func getVersion() string {
 	cmd := exec.Command("git", "describe", "--tags", "--always")
-	cmd.Stderr = os.Stderr
+	buf := new(bytes.Buffer)
+	cmd.Stderr = buf
 	out, err := cmd.Output()
 	if err != nil {
-		log.Fatalf("Error running git describe: %v", err)
+		// log.Println(buf.String())
+		// log.Printf("No git: Error running git describe: %v", err)
+		return ""
 	}
+
 	return strings.TrimPrefix(strings.TrimSpace(string(out)), "v")
 }
 
@@ -120,17 +154,25 @@ func forEachBinTarget(bin binary, fn binaryFunc) {
 // combination while using --ldflags to set the binary's version variable.
 func buildBinary(bin binary, OS, arch string) {
 	ldflags := fmt.Sprintf("--ldflags=-X main.version=%s", bin.version)
-	cmd := exec.Command("go", "build", ldflags, "-o", bin.Name(OS, arch))
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	cmd := exec.Command("go", "build", "-x", ldflags, "-o", rwd+bin.Name(OS, arch))
+	buf := new(bytes.Buffer)
+	cmd.Stdout = buf
+	cmd.Stderr = buf
 
 	cmd.Env = copyOSEnv()
 	cmd.Env = setEnv(cmd.Env, "GOOS", OS)
 	cmd.Env = setEnv(cmd.Env, "GOARCH", arch)
-	fmt.Println("Building binary:", bin.Name(OS, arch))
 	if err := cmd.Run(); err != nil {
-		log.Fatalln("Error running go build:", err)
+		if !strings.Contains(buf.String(), "no buildable Go source files") {
+			log.Println("BUILD ERROR:", err, buf.String())
+		} else {
+			log.Println("fatal: not a Go project")
+			log.Println("usage:", "make.go", "[go_project]")
+		}
+		os.Exit(111)
 	}
+	if *verbose { log.Println(buf.String()) }
+	log.Println("Built:", bin.Name(OS, arch))
 }
 
 // rmBinary removes a binary from the current directory.
@@ -158,4 +200,37 @@ func setEnv(environ []string, key, value string) []string {
 		}
 	}
 	return append(environ, fmt.Sprintf("%s=%s", key, value))
+}
+
+// Name returns the name of the binary with a certain format for a platform.
+func (bin binary) Name(os, arch string) string {
+
+	s := fmt.Sprintf("%s_%s-%s", bin.name, os, arch)
+
+	if bin.version != "" {
+		s = fmt.Sprintf("%s_%s_%s-%s", bin.name, bin.version, os, arch)
+	}
+
+	if os == "windows" {
+		s = s + ".exe"
+	}
+	return s
+}
+
+// Names returns the name of the binary for all the target platforms.
+func (bin binary) Names() []string {
+	names := make([]string, len(bin.targets))
+	for i, t := range bin.targets {
+		names[i] = bin.Name(t.os, t.arch)
+	}
+	return names
+}
+
+
+func getMainProjectName(dir string) string {
+		dir = strings.TrimSuffix(dir, "/")
+		dirname := strings.Split(dir, "/")
+		projectName := dirname[len(dirname)-1]
+		return projectName
+
 }
